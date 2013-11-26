baab4dbc5f14652323457f2f5d822e8c0b40b74f
Vasyl Khomenko <vkhomenko@qubell.com>
2013-11-26 19:29:07 +0200

    Added Installer script

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_requires = [
+    'simplejson',
+    'pyaml',
+    'requests'
+    ]
+
+test_requires = [
+    'testtools',
+    'nose',
+    ]
+
+setup(name='qubellclient',
+      version='0.1',
+      description='Qubell platform client library',
+      long_description=open('README.rst').read(),
+      author='Vasyl Khomenko',
+      author_email='vkhomenko@qubell.com',
+      license='LICENSE',
+      url='https://github.com/vasichkin/contrib-python-qubell-client',
+      packages=find_packages(exclude=['tests*']),
+      include_package_data=True,
+      install_requires=install_requires,
+      tests_require=test_requires,
+      test_suite="nosetests",
+     )
