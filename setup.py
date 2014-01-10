a8cca387d943189ff588fde77bda0651fd0968c9
Anton Panasenko <anton.panasenko@gmail.com>
2014-01-10 16:51:34 +0200

    Fixed version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requires = [
     ]
 
 setup(name='qubell-api-python-client',
-      version='1.0.21',
+      version='1.0.22',
       description='Qubell platform client library',
       long_description=open('README').read(),
       author='Vasyl Khomenko',
