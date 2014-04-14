41a3504cb77b1c94469c23eeb56261786f77f86a
Vasyl Khomenko <vkhomenko@qubell.com>
2014-04-14 13:04:58 +0300

    fix unittest

--- qubell/tests/testing/test_environment_decorator.py
+++ qubell/tests/testing/test_environment_decorator.py
@@ -17,7 +17,7 @@ class EnvironmentDecoratorTests(unittest.TestCase):
 
     def test_apply(self):
         old_tests = ["test_nothing", "test_fail"]
-        new_tests = ["test_nothing_on_environment_a for DummyTests", "test_nothing_on_environment_b for DummyTests", "test_fail_on_environment_a for DummyTests", "test_fail_on_environment_b for DummyTests"]
+        new_tests = ["test_nothing_on_environment_a_for_DummyTests", "test_nothing_on_environment_b_for_DummyTests", "test_fail_on_environment_a_for_DummyTests", "test_fail_on_environment_b_for_DummyTests"]
         clazz = environment({"a":{"A":"AA"}, "b":{"B":"BB"}})(self.DummyTests)
         for name in old_tests:
             assert name not in clazz.__dict__
