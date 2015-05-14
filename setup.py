5a18ff7a3f6aa94b0852cc514d632c3ce1cac646
Vasyl Khomenko <vkhomenko@qubell.com>
2015-05-14 21:28:31 +0300

    Up version 1.39.41.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requires = [
     ]
 
 setup(name='qubell-api-python-client',
-      version='1.38.39.1',  # versionising: <major>.<minor>.<platform major>.<platform minor>
+      version='1.39.41.1',  # versionising: <major>.<minor>.<platform major>.<platform minor>
       description='Qubell platform client library',
       long_description=open('README').read(),
       author='Vasyl Khomenko',
