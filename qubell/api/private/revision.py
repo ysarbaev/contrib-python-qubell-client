a3af7846b3bdd7bfa5b2b4e55c3ae0053c297f55
Yuri Sarbaev <yuri.sarbaev@gmail.com>
2015-11-19 11:31:36 +0300

    force remove paramter for delete_revision

--- qubell/api/private/revision.py
+++ qubell/api/private/revision.py
@@ -61,9 +61,11 @@ class Revision(Entity, InstanceRouter):
     def json(self):
         return self._router.get_revision(org_id=self.organizationId, app_id=self.applicationId, rev_id=self.id).json()
 
-    def delete(self):
-        self._router.delete_revision(org_id=self.organizationId, app_id=self.applicationId, rev_id=self.id)
-        return True
+    def delete(self, force=True):
+        if force:
+            return self._router.delete_revision(org_id=self.organizationId, app_id=self.applicationId, rev_id=self.id, force="true")
+        else:
+            return self._router.delete_revision(org_id=self.organizationId, app_id=self.applicationId, rev_id=self.id, force="false")
 
 
 class RevisionList(EntityList, InstanceRouter):
